084e3f9fb6e8675a70839c35d57a076efda3655c
Tim Rourke <tim@timrourke.com>
2019-04-08 10:12:22 -0500

    Gut default app

---
+++ src/components/Popover.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Popover extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
