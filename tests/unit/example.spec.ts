5ec8bed27ff0b3589b4e3302ca535b7345349d42
Tim Rourke <tim@timrourke.com>
2019-04-08 10:14:47 -0500

    Update unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Popover from '@/components/Popover.vue';
 
-describe('HelloWorld.vue', () => {
+describe('Popover.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Popover, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
